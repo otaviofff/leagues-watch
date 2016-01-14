bc2f764ad21403271e450475debfeb9e97c4ecd2
Otavio Ferreira <otavio.ferreira@gmail.com>
2016-01-14 12:17:20 -0200

    Now using Raviolini v0.0.4. File pom.xml updated accordingly.

--- src/main/java/watch/leagues/api/FrontController.java
+++ src/main/java/watch/leagues/api/FrontController.java
@@ -1,23 +1,18 @@
 package watch.leagues.api;
 
-import org.raviolini.api.RequestRouter;
+import org.raviolini.api.AbstractController;
 
 import watch.leagues.domain.News;
 import watch.leagues.domain.NewsService;
 import watch.leagues.domain.Subscription;
 import watch.leagues.domain.SubscriptionService;
 
-public class FrontController {
+public class FrontController extends AbstractController {
 
     public static void main(String[] args) {
-        RequestRouter<News> router1 = new RequestRouter<>();
+        listenToAssignedPort();
         
-        router1.override(new NewsService());
-        router1.route(News.class);
-        
-        RequestRouter<Subscription> router2 = new RequestRouter<>();
-        
-        router2.override(new SubscriptionService());
-        router2.route(Subscription.class);
+        addRouter(News.class, new NewsService());
+        addRouter(Subscription.class, new SubscriptionService());
     }
 }
